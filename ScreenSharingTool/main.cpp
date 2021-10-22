a47ee54c377bb42710620c980b723862f6ec9a68
magicfairyking <2452502323@qq.com>
2021-10-22 17:36:48 +0800

    add init ffmpeg

--- ScreenSharingTool/main.cpp
+++ ScreenSharingTool/main.cpp
@@ -1,8 +1,10 @@
 #include "MainWindow.h"
+#include "ffmpeg.h"
 #include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
+    ffmpeg_init();
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
